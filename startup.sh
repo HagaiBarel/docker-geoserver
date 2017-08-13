4678328ab666faf4c784a45ef192f2c7c4c99761
Hagai Barel <hagaib@codevalue.net>
2017-08-13 18:15:21 +0300

    added geoserver data dir var to java_opts

--- startup.sh
+++ startup.sh
@@ -2,6 +2,6 @@
 
 export INSTANCE_NAME=$HOSTNAME
 export CLUSTER_CONFIG_DIR=${GEOSERVER_DATA_DIR}/cluster/${INSTANCE_NAME}
-export JAVA_OPTS='-DCLUSTER_CONFIG_DIR=$CLUSTER_CONFIG_DIR -Dactivemq.base=$CLUSTER_CONFIG_DIR/tmp -Dactivemq.transportConnectors.server.uri=tcp://127.0.0.1:9545'
+export JAVA_OPTS='-DGEOSERVER_DATA_DIR=$GEOSERVER_DATA_DIR -DCLUSTER_CONFIG_DIR=$CLUSTER_CONFIG_DIR -Dactivemq.base=$CLUSTER_CONFIG_DIR/tmp -Dactivemq.transportConnectors.server.uri=tcp://127.0.0.1:9545'
 
 /bin/bash -c "catalina.sh run"
